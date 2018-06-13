87b5f53471ef553262776a40244169d6325985e7
Jnthnss <38191190+Jnthnss@users.noreply.github.com>
2018-06-13 16:28:20 -0700

    Added sort_values
    Added sort_values to Reachable and Not Reachable filters to order by ascending and descending. Also included options for No Match and Duplicates but left them out unless requested. Removed 'path' variable.

--- updatedscript.py
+++ updatedscript.py
@@ -9,43 +9,45 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 # Establish original csv to read from: "base"
-base = pd.read_csv('test.csv')
+base = pd.read_csv('Cisco NVS File 6_11_2018-06-11_124799_output.csv')
 
 # Establish range of columns needed - in this script will need to keep match type, column #8
 length = len(base.columns)
 pos = [0, 1, 2, 8, 9, 10, 19, 28]
 pos2 = range(35, length)
-colnames = [0, 1, 2, 8, 9, 10, 19, 28] & [range(35, length)]
+colnames = pos + pos2
 
-final = pd.read_csv('test.csv', usecols = colnames)
+final = pd.read_csv('Cisco NVS File 6_11_2018-06-11_124799_output.csv', usecols = colnames)
 
 # Filter and then create new dataframe to create new tab in xls 
 reachable_filter = (final["MEI"] >= 25) & (final["active"] == True) 
 reachable_df = final[reachable_filter]
+reachable_sort = reachable_df.sort_values("MEI", inplace = False)
 
 not_reachable_filter = (final["MEI"] < 25) & (final["active"] == True)
 not_reachable_df = final[not_reachable_filter]
+not_reachable_sort = not_reachable_df.sort_values("MEI", inplace = False, ascending = False)
 
 # No matches can also be duplicates so need to figure out a way that eliminates the duplicate rows from no match tab
 no_match_filter = (final["active"] == False) 
-no_match_df = final[no_match_filter]
+temp_df = final[no_match_filter]
 # Filter out two criteria from dataframe by placing both criteria in () brackets 
-no_dupes_df = no_match_df[(no_match_df["Match Type"] != "duplicate match") & (no_match_df["Match Type"] != "duplicate input")]
+no_match_df = temp_df[(temp_df["Match Type"] != "duplicate match") & (temp_df["Match Type"] != "duplicate input")]
+# no_match_sort = no_match_df.sort_values("MEI", inplace = False, ascending = False)
 
-# Created this to remove duplicate SIDs 
+# Created this to place duplicate results in duplicates tab
 duplicate_filter = (final["active"] == False) & (final["Match Type"] == "duplicate match") | (final["Match Type"] == "duplicate input")
-duplicate_df = final[duplicate_filter] 
+duplicate_df = final[duplicate_filter]
+# duplicate_sort = duplicate_df.sort_values("MEI", inplace = False, ascending = False) 
 
 # Set new file path/directory for newly created xls files
-path = "/Users/jshek/Desktop/completed xls"
-os.chdir(path)
+os.chdir("/Users/jshek/Desktop/completed xls")
 
 from pandas import ExcelWriter
-writer = pd.ExcelWriter('test.xlsx', engine = "xlsxwriter")
+writer = pd.ExcelWriter('Cisco NVS File 6_11_2018-06-11_124799_output.xlsx', engine = "xlsxwriter")
 
-reachable_df.to_excel(writer, sheet_name = 'Reachable', index = False)
-not_reachable_df.to_excel(writer, sheet_name = 'Not Reachable', index = False)
-no_dupes_df.to_excel(writer, sheet_name = 'No Match', index = False)
+reachable_sort.to_excel(writer, sheet_name = 'Reachable', index = False)
+not_reachable_sort.to_excel(writer, sheet_name = 'Not Reachable', index = False)
+# only reachable and not reachable sorted by MEI 
+no_match_df.to_excel(writer, sheet_name = 'No Match', index = False)
 duplicate_df.to_excel(writer, sheet_name = 'Duplicates', index = False)
-
-
